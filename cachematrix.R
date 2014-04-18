71243bd47fb6ed599e5fb066bb422b8b072487d1
E. Valencia <valenzia10@gmail.com>
2014-04-18 10:15:38 +0200

    Updated function descriptions and interfaces.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cacheMatrix.R
+## Author: E. Valencia
+## April 2014
+##
+## The purpose of these functions is to provide an object
+## to store the inverse of a matrix in order to cache it
+## so as not to compute it again once it has been computed
+## for the first time.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix
+## Creates an object that stores a matrix, its inverse,
+## and provides a set of functions to set/get them.
+##
+## Arguments: x - Invertible matrix
+## Return: A cacheMatrix object 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## Checks whether the inverse of a matrix stored in a cacheMatrix
+## object has already been computed and returns it. If not, 
+## the matrix inverse is computed and stored within the
+## cacheMatrix object.
+##
+## Arguments: x - A cacheMatrix object
+## Return: The inverse of the matrix within the provided
+##         cacheMatrix object. 
+cacheSolve <- function(x) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
